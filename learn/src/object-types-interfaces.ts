671ae7bfbf0b45fefc8dbde5412d79fe27920a23
xThgSilva <camposthiago2021@gmail.com>
2026-08-31 23:59:01 -0300

    feat: discriminated unions and declaration merging

--- learn/src/object-types-interfaces.ts
+++ learn/src/object-types-interfaces.ts
@@ -29,14 +29,14 @@ interface Person {
     age: number;
 };
 
-// Using Car interface
+// Using Person interface
 const person: Person = {
     name: "person name",
     age: 25
 };
 
 // Extends between interfaces
-interface Admin extends Person {
+interface Manager extends Person {
     role: string
 };
 
@@ -81,3 +81,66 @@ const myBook: Book = {
     printLength: 1950
 };
 
+// Discriminated Unions
+type Admin = {
+    role: "admin",  // Literal type, used as a discriminator
+    name: string,
+    permissions: string[]
+};
+
+type Customer = {
+    role: "customer",   // Literal type, used as a discriminator
+    name: string,
+    purchases: number
+};
+
+type User = Admin | Customer;
+
+function printUser(user: User) {
+    // Using discriminator in narrowing
+    if (user.role === "admin") {
+        console.log(user.permissions);
+    } else {
+        console.log(user.purchases);
+    }
+}
+
+// Useful example
+type RequestState = 
+{ status: "loading"; } | { status: "success"; data: string; } | { status: "error"; error: string; };
+// Now TypeScript know exactly each situation
+function handleRequest(state: RequestState) {
+    if (state.status === "loading") {
+        console.log("Loading...");
+    }
+    if (state.status === "success") {
+        console.log(state.data);
+    }
+    if (state.status === "error") {
+        console.log(state.error);
+    }
+}
+
+// Exhaustiveness Checking
+type PaymentStatus = "pending" | "approved" | "rejected";
+
+function handlePayment(status: PaymentStatus) {
+    switch (status) {
+        case "pending":
+            console.log("Payment is pending");
+            break;
+
+        case "approved":
+            console.log("Payment approved");
+            break;
+
+        case "rejected":
+            console.log("Payment rejected");
+            break;
+
+        default:
+           // TypeScript will warn us if a new PaymentStatus type is added and not handled case new type of "PaymentStatus" is added for exceptions
+            const exhaustiveCheck: never = status;
+            return exhaustiveCheck;
+    }
+}
