9b2e71cd2cd5cfcb652f885f1efadedaa200b01e
xThgSilva <camposthiago2021@gmail.com>
2026-09-06 22:41:14 -0300

    feat: generic constraints with keyof, generics types and generics interfaces

--- learn/src/generics.ts
+++ learn/src/generics.ts
@@ -13,3 +13,53 @@ interface Lengthwise {
 function getLength<T extends Lengthwise>(value: T): number {
     return value.length;
 }
+
+// Generics Constraints + keyof
+function getProperty<T, K extends keyof T>(object: T, key: K) {
+    // K has to be one of the keys of T
+    return object[key];
+}
+
+type Product = {
+    readonly id: number,
+    name: string;
+    price: number;
+}
+
+const product: Product = {
+    id: 10,
+    name: "TV",
+    price: 999.99
+}
+
+console.log(getProperty(product, "name")) // Return "TV". T = product and K = keyof product ("id" | "name" | "price")
+
+// Generic Types and Interfaces
+type Box<T> = {
+    value: T
+}
+
+// Can create any object value to "value" property in "Box"
+const numberBox: Box<number> = {
+    value: 56   // Need to be a number type
+}
+
+const stringBox: Box<string> = {
+    value: "Now is a string box (value is string type)"
+}
+
+interface ApiResponse<T> {
+    data: T,
+    status: number
+}
+
+const response: ApiResponse<string> = {
+    data: "Success response",
+    status: 200
+}
+
+const response2: ApiResponse<number> = {
+    data: 1000,
+    status: 200
+}
+
